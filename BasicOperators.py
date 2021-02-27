10a332510740eaf1e1fc752b2b61b907abde7967
charliemedina1 <78857735+charliemedina1@users.noreply.github.com>
2021-02-27 15:41:15 +0000

    Add files via upload

---
+++ BasicOperators.py
@@ -0,0 +1,17 @@
+x = object() #define variable
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10 # repeats  x in list 10 times
+y_list = [y] *10
+big_list = x_list + y_list #add lists together
+
+print("x_list contains %d objects" % len(x_list)) #check length og list
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
